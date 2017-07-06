d696cc99e2090d8321f4ff7865e92ccd45820148
uzername <ivanpost777@gmail.com>
2017-07-06 09:28:56 +0300

    Main file...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <cstdlib>
+#include <cstdio>
 #include "tty.h"
 using namespace std;
 int main(int argc, char** argv) {
+    printf("===SIM5320E COM PORT WINDOWS UTIL===\n");
+    //add comport enumeration using enumser module
     tty* COMportInstance = new tty();
+    printf("port: ");
+    char * portName;
+    scanf();
     return 0;
 }
 
